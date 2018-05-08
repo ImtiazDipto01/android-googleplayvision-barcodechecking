88ae12db33ba371e42670b519cfbe7039a8d8599
imtiaz <programmer.imtiaz@gmail.com>
2018-05-08 19:16:55 +0600

    ADD- camera permission added

--- app/src/main/java/com/example/annanovas/barcodechecking/NewMainActivity.java
+++ app/src/main/java/com/example/annanovas/barcodechecking/NewMainActivity.java
@@ -1,9 +1,17 @@
 package com.example.annanovas.barcodechecking;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +23,9 @@ public class NewMainActivity extends AppCompatActivity {
 
     Button scan ;
     TextView tvReuslt ;
+    // permission request codes need to be < 256
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
+    public String TAG = "NewMainActivity" ;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,8 +38,16 @@ public class NewMainActivity extends AppCompatActivity {
         scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(NewMainActivity.this, ScanBarcodeActivity.class);
-                startActivityForResult(intent, 0);
+                int rc = ActivityCompat.checkSelfPermission(NewMainActivity.this, Manifest.permission.CAMERA);
+                if (rc == PackageManager.PERMISSION_GRANTED) {
+                    //createCameraSource(autoFocus, useFlash);
+                    Intent intent = new Intent(NewMainActivity.this, ScanBarcodeActivity.class);
+                    startActivityForResult(intent, 0);
+
+                } else {
+                    requestCameraPermission();
+                }
+
 
             }
         });
@@ -50,5 +69,66 @@ public class NewMainActivity extends AppCompatActivity {
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void requestCameraPermission() {
+        //Log.w(TAG, "Camera permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+
+        final Activity thisActivity = this;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(thisActivity, permissions,
+                        RC_HANDLE_CAMERA_PERM);
+            }
+        };
+
+        /*Snackbar.make(mGraphicOverlay, R.string.permission_camera_rationale,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, listener)
+                .show();*/
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != RC_HANDLE_CAMERA_PERM) {
+            Log.d(TAG, "Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Camera permission granted - initialize the camera source");
+            // we have permission, so create the camerasource
+            Intent intent = new Intent(NewMainActivity.this, ScanBarcodeActivity.class);
+            startActivityForResult(intent, 0);
+            return;
+        }
+
+        Log.e(TAG, "Permission not granted: results len = " + grantResults.length +
+                " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Multitracker sample")
+                .setMessage(R.string.no_camera_permission)
+                .setPositiveButton(R.string.ok, listener)
+                .show();
     }
 }
--- app/src/main/java/com/example/annanovas/barcodechecking/ScanBarcodeActivity.java
+++ app/src/main/java/com/example/annanovas/barcodechecking/ScanBarcodeActivity.java
@@ -2,10 +2,13 @@ package com.example.annanovas.barcodechecking;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -29,16 +32,18 @@ public class ScanBarcodeActivity extends Activity {
     // permission request codes need to be < 256
     private static final int RC_HANDLE_CAMERA_PERM = 2;
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_barcode);
 
         cameraPreview = (SurfaceView) findViewById(R.id.camera_preview);
+        createCameraSource();
         int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
         if (rc == PackageManager.PERMISSION_GRANTED) {
             //createCameraSource(autoFocus, useFlash);
-            createCameraSource();
+
         } else {
             requestCameraPermission();
         }
@@ -131,4 +136,6 @@ public class ScanBarcodeActivity extends Activity {
                 .setAction(R.string.ok, listener)
                 .show();*/
     }
+
+
 }
